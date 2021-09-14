7d0602d129608ec93b5b4aa6a51514835fe0d3e1
kondraschovAV1999 <kondrascho.andrew1999@gmail.com>
2021-09-14 09:20:56 +0300

    The lab1 is completed

--- LR1.2.py
+++ LR1.2.py
@@ -6,46 +6,48 @@ import colorama as color
 
 
 def inputDigit(maxDigit, text):  # Функция для проверки правильности ввода чисел
-    newAttempt = True
+    # Параметры функции: maxDigit - предел по значению, вводимых чисел; text - текст выводимый на экран
+    newAttempt = True  # Маркер, указывающий продолжение ввода
     while newAttempt:
         inDigit = input(color.Fore.LIGHTCYAN_EX + text + color.Style.RESET_ALL)
-        if inDigit.isdigit():
+        if inDigit.isdigit():  # Проверка что введено число, если это так, то преобразуем строку в число
             outDigit = int(inDigit)
             if outDigit <= maxDigit:
                 newAttempt = False
             else:
                 print(color.Fore.RED + "Недопустимое числовое значение!" + color.Style.RESET_ALL)
-        else:
+        else:  # Если нет, то идет следующая попытка ввода
             print(color.Fore.RED + "Введено не число!" + color.Style.RESET_ALL)
     return outDigit
 
 
-def graph(title, y, x):
-    figure1 = plt.figure(figsize=(7, 4))
-    axes1 = figure1.add_subplot(1, 1, 1)
-    axes1.plot(x, y)
-    axes1.grid(True)
-    axes1.set_title(title)
-    axes1.set_xlabel('Время')
-    axes1.set_ylabel('Амплитуда')
+def graph(title, y, x):  # Функция для построения отдельного графика
+    figure1 = plt.figure(figsize=(7, 4))   # Создается объект класса Figure(по сути это холст, на котороым рисуем)
+    axes1 = figure1.add_subplot(1, 1, 1)  # Создаем рабочую область на который будут оси (изображаем на холсте)
+    axes1.plot(x, y)  # Строится сам график уже внутри рабочей области
+    axes1.grid(True)  # Включение сетки
+    axes1.set_title(title)  # Наименование графика
+    axes1.set_xlabel('Время')   # Устанавливается подпись оси x
+    axes1.set_ylabel('Амплитуда')  # Устанавливается подпись оси у
 
 
-def action(link):
-    timeLine = []
+def action(link):  # Функция для построения всех графиков
+    timeLine = []  # Список точек по времени
     for i in range(0, 10000):
         timeLine.append(i / 1000)
-    [y, x] = matlab.step(link, timeLine)
+    [y, x] = matlab.step(link, timeLine)  # Получение переходной характеристики
     graph('Переходная характеристика', y, x)
     [y, x] = matlab.impulse(link, timeLine)
-    graph("Импусльная характеристика", y, x)
-    mag, phase, omega = matlab.freqresp(link, timeLine)
+    graph("Импусльная характеристика", y, x)  # Получение импульсной характеристики
+    mag, phase, omega = matlab.freqresp(link, timeLine)  # Получение АЧХ, ФЧХ
     graph("АЧХ", mag, timeLine)
     graph("ФЧХ", phase * 180 / math.pi, timeLine)
     plt.show()
 
 
-class Link:
+class Link:  # Создаем класс "Звено"
     def __init__(self):
+        # Имеет четыре параметра name - тип звена, k и t характеризуют передаточную функцию и tf - передаточная функция
         self.name = inputDigit(5, "Выберете звено:\n"
                                "1 - Безынерционное звено\n"
                                "2 - Апериодическое звено\n"
@@ -74,6 +76,6 @@ class Link:
             self.tf = matlab.tf([self.k, 0], [self.t, 1])
 
 
-link1 = Link()
-tf = link1.tf
-action(tf)
+link1 = Link()  # Создается объект класса Link
+tf = link1.tf  # Получаем передаточную функцию
+action(tf)  # Построение всех графиков
